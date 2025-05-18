c53aa0e16c20178aeda9692868f641968a49592f
REDaHEAD223 <86076419+REDaHEAD223@users.noreply.github.com>
2025-05-18 05:07:26 +0300

    Update index.js
    Signed-off-by: REDaHEAD223 <86076419+REDaHEAD223@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', port)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(`Hello from ззode.js app in OpenShift!\nAPI_KEY: ${process.env.API_KEY}`)
+  response.send(`Hello from ззode.js app in OpenShift!\nAPI_KEY: ${process.env.API_KEY1}`)
 })
 
 app.listen(app.get('port'), function() {
